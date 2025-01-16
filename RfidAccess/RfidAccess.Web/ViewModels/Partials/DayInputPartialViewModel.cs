063b0b072621eb30987a3a04be55a21e205ca3ae
Kristijan <jankuloski.k@gmail.com>
2025-01-16 13:07:17 +0100

    Scheguel optimizations

---
+++ RfidAccess/RfidAccess.Web/ViewModels/Partials/DayInputPartialViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RfidAccess.Web.ViewModels.Schedule;
+
+namespace RfidAccess.Web.ViewModels.Partials
+{
+    public class DayInputPartialViewModel
+    {
+        public DayInputPartialViewModel(List<TimeSlot> daySlots, string dayName, string title)
+        {
+            DaySlots = daySlots;
+            DayName = dayName;
+            Title = title;
+        }
+
+        public List<TimeSlot> DaySlots { get; set; } = [];
+
+        public string DayName { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+    }
+}
